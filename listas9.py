dff1a722abfe64366875bda2b1c534ff6c9ce00a
jducuaram99 <jonathanducuara47@gmail.com>
2025-03-21 14:22:06 -0500

    Add files via upload

---
+++ listas9.py
@@ -0,0 +1,9 @@
+import random
+lista=[random.randint(1,100)for i in range(random.randint(5,15))]
+print(lista)
+Uname="universidad de cundinamarca"
+otra=[10,20,30]
+listaU=[letra for letra in Uname]
+print(listaU)
+listaU=[x for x in otra]
+print(listaU)
